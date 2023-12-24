f3826833d6d9a4e539d1209b632e86a13ac45d5d
Ryan Back <ho0405@gmail.com>
2023-12-23 18:08:28 -0700

    done for navbar, doing mainpage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputerCanvas } from './canvas'
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>Hello, I'm <span className='text-[#915eff]'>Ryan</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            A future AI wizard <br className='sm:block hidden' />currently conjuring code magic<br className='sm:block hidden' />at SAIT in the realm of Software Development!
+
+          </p>
+        </div>
+
+        <ComputerCanvas />
+      </div>
+
+    </section>
   )
 }
 
