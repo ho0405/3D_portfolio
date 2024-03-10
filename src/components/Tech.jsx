d57f0c19a4d4bb07fa5cc11419583c760838a3e5
Ryan Back <ho0405@gmail.com>
2024-03-10 12:59:10 -0600

    updates projects (fix minor issues,delete ballcanvas)

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
@@ -8,7 +7,7 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center">{
       technologies.map((technology, index) => (
         <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+
         </div>
       ))}
     </div>
